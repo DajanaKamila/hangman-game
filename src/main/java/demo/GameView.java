de4384f70d3f2917c47a42045a14ebd4099e663c
DajanaKD <dominiakdajana@wp.pl>
2023-07-07 16:16:26 +0200

    I added game view, game controller, updated game demo

---
+++ src/main/java/demo/GameView.java
@@ -0,0 +1,98 @@
+package demo;
+
+import java.util.List;
+
+public class GameView {
+	
+    public static final String[] HANGMANPICS = new String[]{
+            "  +---+\n" +
+                    "  |   |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "=========",
+            "  +---+\n" +
+                    "  |   |\n" +
+                    "  O   |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "=========",
+            "  +---+\n" +
+                    "  |   |\n" +
+                    "  O   |\n" +
+                    "  |   |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "=========",
+            "  +---+\n" +
+                    "  |   |\n" +
+                    "  O   |\n" +
+                    " /|   |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "=========",
+            "  +---+\n" +
+                    "  |   |\n" +
+                    "  O   |\n" +
+                    " /|\\  |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "=========",
+            "  +---+\n" +
+                    "  |   |\n" +
+                    "  O   |\n" +
+                    " /|\\  |\n" +
+                    " /    |\n" +
+                    "      |\n" +
+                    "=========",
+            "  +---+\n" +
+                    "  |   |\n" +
+                    "  O   |\n" +
+                    " /|\\  |\n" +
+                    " / \\  |\n" +
+                    "      |\n" +
+                    "========"
+    };
+	
+	public void printHangman(int incorrectAnswers) {
+		System.out.println(HANGMANPICS[incorrectAnswers]);
+	}
+	
+	public void printMenu() {
+		System.out.println("Welcome to the Hangman Gamee! \n"
+				+ "1. Start game \n2. Options");
+	}
+	
+	public void printInvalidData() {
+		System.out.println("Sorry, invalid data.");
+	}
+	
+	public void printUnimplementedOption() {
+		System.out.println("Sorry! This page in unavailable now :(");
+	}
+	
+	public void printWordToGuess(StringBuilder wordToGuess) {
+		System.out.println(wordToGuess);
+	}
+	
+	public void printWinner() {
+		System.out.println("Congrats! You win!");
+	}
+	
+	public void printIncorrectAnswer() {
+		System.out.println("Upsss! Wrong answer. Try again!");
+	}
+	
+	
+	public void printGuessedLetters(List<String> guessedLetters) {
+		System.out.println("To date you typed those letters: " + guessedLetters.toString().replace(",", ""));
+	}
+	
+	public void printLose() {
+		System.out.println("Sorry, you lose! Good luck next time!");
+	}
+	
+	
+}
